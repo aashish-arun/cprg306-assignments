7d98a031eac79e50d525973f64a7f22d36ce48fc
FirstOfLast <aashish.ouo@gmail.com>
2025-09-19 12:12:03 -0600

    Completed assignment week-2

---
+++ app/week-2/student-info.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Link from "next/link";
+
+function StudentInfo() {
+  return (
+    <div>
+      <h1>Aashish Arun</h1>
+      <Link href="https://github.com/aashish-arun/cprg306-assignments">My GitHub Repository</Link>
+    </div>
+  );
+}
+
+export default StudentInfo;
