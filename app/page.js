af777884d7cec9be612859bee0f6d34b80999384
FirstOfLast <aashish.ouo@gmail.com>
2025-09-26 13:20:33 -0600

    Week-3

--- app/page.js
+++ app/page.js
@@ -5,7 +5,11 @@ export default function Home() {
     <main>
       <h1>CPRG 306: Web Development 2 - Assignments</h1>
       <Link href="/week-2">
-        Week-2
+        Week-2  
+      </Link>
+      
+      <Link href="/week-3">
+         → Week-3
       </Link>
     </main>
   );
